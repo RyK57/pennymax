efc4a1feadc2a24e84176f8713505f70266e7083
RyK57 <rithviks.ryk57@gmail.com>
2026-03-24 16:04:49 -0700

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter, Geist } from 'next/font/google'
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className={inter.className}>{children}</body>
     </html>
   )
